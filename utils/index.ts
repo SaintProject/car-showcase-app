48540fa3ee9da5cf0cde3eed0cfca6efc26ef4a2
SaintProject <136699678+SaintProject@users.noreply.github.com>
2023-11-23 23:40:45 +0200

    fixed env syntax

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,7 @@ export async function fetchCars(filters: FilterProps) {
     const { model, manufacturer, year, fuel, limit } = filters;
 
     const headers= {
-		'X-RapidAPI-Key': process.env.RAPID_API_KEY || "",
+		'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
